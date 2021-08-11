743c83add3a077e33ca66b083dca29d093e4ee69
Debshankar GHose <debshankar29@gmail.com>
2021-08-10 23:03:49 -0500

    committing on 07/31 for updates made to palindrome check via string to char conversion.

--- testJavaExamples/Palindrome.java
+++ testJavaExamples/Palindrome.java
@@ -1,64 +1,46 @@
-//import java.util.Arrays; 
-/*\
+//import java.util.Arrays;
+/*
  * if java.util.Arrays package is imported, then we print the array in an array format
  * with comma for the class Arrays.toString()
- */
 
-/*
  * Here is another way to verify in a complete way by reversing the string to see,
  * if the string is a palindrome.
- * 
  */
 
-public class Palindrome {
-	
-	public static void main(String args[])
-	{
+import java.util.Arrays;
 
-	
+public class Palindrome {
+	public static void main(String[] args)
+	{
 		String p = "malayalam";
 		char[] arry = p.toCharArray();
-		
-		
-		System.out.print("String before Reversing: ");
-		System.out.println(arry);
-		
-		int len = arry.length;
-		//System.out.println(len);
+		System.out.println("String before Reversing: "+ Arrays.toString(arry));
+		int len = arry.length; //assigns the size of array to the variable.
 		char[] tmp = new char[len];
-		//System.out.println(tmp);
-
-//int i,j;
-		
-		for(int i=len-1, j =0; i>=0 && j<=len-1; i-- , j++) 
+		//int i,j; // either can initialise variable i and j or directly below in for loop
+		for(int i=len-1, j =0; i>=0 && j<=len-1; i--, j++)
 		{
-			
-				tmp[j] = arry[i];
-				//System.out.println(tmp);
+			tmp[j] = arry[i];
+			//System.out.println(tmp);
 			}
-
-			//String rev = new String(tmp,2,7); 
+		//String rev = new String(tmp,2,7);
 		/*
 		 * just to check what part of the string to be returned from starting index
 		 * which is 2 and counting 7 from index 2
 		 */
 		
 //Multiple ways of converting char array to string
-			//String rev1 = new String(tmp);
-		 String rev = String.valueOf(tmp); 
-			//String rev = Arrays.toString(tmp);
-			System.out.println(rev);
-				
-			System.out.println("String after reversing: " +rev);
-			
-			if (p == rev) {
+			String rev1 = new String(tmp); /*or we can have the string with char array, if
+											we write //String rev2 = Arrays.toString(tmp); */
+		 	//String rev = String.valueOf(tmp); //this statement can also work in converting char array to String
+			System.out.println(rev1);
+			System.out.println("String after reversing: " +rev1);
+			if (p.equals(rev1)) { /* here if(p==rev1), will not work,since == is an operator and compares the address
+									reference, not the content, however .equals() compares the content */
 				System.out.println("The String is palindrome");
+			}else {
+				System.out.println("The string is not a palindrome");
 			}
-			System.out.println("The string is not a palindrome");
-		
-		
-			
-
 	}
 }
 
